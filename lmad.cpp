39a1599d6e23a2bfea71b34e5c3807eaf87788a3
Bobiticmo <57601174+Bobiticmo@users.noreply.github.com>
2019-11-14 10:55:21 -0600

    Update lmad.cpp
    Modificar da error y crear nota pero ya es mas de idea general

--- lmad.cpp
+++ lmad.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <fstream>
-#include <string>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sstream>
@@ -144,234 +144,148 @@ string Consultar() {
 	menu();
 }
 
-string Eliminar() {
-	system("cls");
-	int op1;
-	string line,iid;
-	cout << "quierer borrar [1]-subservicios o un [2]-servicio completo" << endl;
-	cin >> op1;
-	fflush(stdin);
-	if(op1==2){
-		ifstream archivo;
-		archivo.open("Subservicio.txt");
-		ofstream temp;
-		temp.open("Temp.txt");
-		cout << "cual es la ID del servicio relacionado";
-		cin >> iid;	
-		fflush(stdin);
-		while(archivo.eof()){
-			while(getline(archivo,line))
-			{
-				line.replace(line.find(iid),iid.length(),"");
-    			temp << line << endl;
-			}
-		}
-		
-		archivo.close();
-		temp.close();
-		remove("Subservicio.txt");
-		rename("temp.txt","Subservicio.txt");
-	}
-	if(op1==1){
-		ifstream archivo;
-		archivo.open("Servicio.txt");
-		ofstream temp;
-		temp.open("Temp.txt");
-		cout << "cual es la ID del servicio relacionado";
-		cin >> iid;
-		fflush(stdin);
-		while(archivo.eof()){
-			while(getline(archivo,line)){
-				line.replace(line.find(iid),iid.length(),"");
-				temp << line << endl;
-			}
-		}
-		archivo.close();
-		temp.close();
-		remove("Servicio.txt");
-		rename("temp.txt","Servicio.txt");
-	}
-	actualizar();
-	menu();
-}
-
-void actualizar(){
-	string line,line2;
-	ifstream servicios;
-	servicios.open("Servicios.txt");
-	ifstream subservicios;
-	subservicios.open("Subservicios.txt");
-	ofstream temp;
-	temp.open("temp.txt");
-	while(servicios.eof()){
-		while(getline(servicios,line)){
-			temp << line << endl;
-			while(subservicios.eof()){
-				while(getline(subservicios,line2)){
-					temp << line2 << endl;
-				}
-			}
-		}
-	}
-	servicios.close();
-	subservicios.close();
-	temp.close();
-	remove("Agregar.txt");
-	rename("temp.txt","Agregar.txt");
+string Eliminar() {/////////////////////////////////////////////////////////////////////////////////////7
 	
 }
 
-string Modificar() {
+string Modificar(){
+	int ops1,ops2,ID,SubID,precio,strAsInt;;
 	system("cls");
-	int ID, Bclave, IDSub, Sclave, op1, op2;
-	char Nombre[40], NewN[40], NSub[20], NewS[20];
-	float precio, P$, PSub, NP$;
-	ifstream archivo;
-	ofstream Temp;
-
-	cout << "Ha escogido la opcion de Modificar Servicios" << endl;
-	archivo.open("Agregar.txt", ios::app);
-	Temp.open("Temp.txt");
-	archivo >> Nombre;
-	bool encontrado = false;
-	cout << "Ingresar la ID del servicio para modificarlo" << endl;
-	cin >> Bclave;
-	fflush(stdin);
-
-	while (archivo.eof()) {
-		archivo >> ID;
-		if (ID == Bclave); {
-			encontrado = true;
-			cout << "El nombre es..." << Nombre << endl;
-			cout << "El ID es..." << ID << endl;
-			cout << endl << endl;
-			system("cls");
-			cout << "Ingrese el nuevo nombre" << endl;
-			cin >> NewN;
-			fflush(stdin);
-			 }
-		Temp << NewN << "" << ID << endl;
-		Temp << P$ << "" << ID << endl;
-		cout << "Modificado" << endl;
-	}
-
-	archivo >> Nombre;
-	if (encontrado = false) {
-		cout << "El ID no fue encontrado" << endl;
-	}
-	cout << "Presiona cualquier tecla para continuar" << endl;
-	cin >> op1;
-	fflush(stdin);
-	system("cls");
-
-	cout << "Tambien quiere modificar los subservicios" << endl;
-	cout << "1.Si\n2.No" << endl;
-	cin >> op2;
-	system("cls");
-	if (op2 == 1) {
-		archivo >> NSub;
-		bool encontrado = false;
-		cout << "Ingrese el ID del subservicio que quiere modificar" << endl;
-		cin >> Sclave;
+	cout << " por razones de sentido programacional editar un servicio ha sido desabilitado"<< endl;
+	do{
+		cout << " elige la ID que rige al SubServicio a Modificar"<<endl;
+		cin >> ops1;
 		fflush(stdin);
-		cout << "Ingrese el precio del producto" << endl;
-		cin >> PSub;
-		fflush(stdin);
-		cout << "Modificado..." << endl;
-     }
-	archivo >> NSub;
-	if (encontrado = false) {
-		cout << "El ID no se encontro" << endl;
+		if(ops1<1000 || ops1>9999)
+			cout << " La ID que rige es de 4 digitos ";
+	}while(ops1<1000 || ops1>9999);
+	fstream archivo ("Subservicio.txt",ios::in|ios::out);
+	while(!archivo.eof()){
+		string s[4];
+		string line,Name;
+		int pos = archivo.tellg();
+		getline (archivo , line);
+		if (line.length()>0){
+			stringstream sline(line);
+			for(int i=0;i<4;i++){
+				getline(sline,s[i],',');
+				if(i=0){
+					sline>>strAsInt;
+				}
+			}
+			if(strAsInt==ops1){
+				archivo.seekp(pos);
+				cout <<"Elige que opcion quieres modificar \n[1]-ID del servicio \n[2]-SubID\n[3]-Nombre\n[4]-precio"<<endl;
+				cin >>ops2;
+				fflush(stdin);
+				switch(ops2){
+					case 1:{
+						do{
+							cout << "escribe por que quieres cambiar el ID del servicio"<<endl;
+							cin >> ID;
+							fflush(stdin);
+							if(ID<1000 || ID>9999)
+								cout << " La ID que rige es de 4 digitos ";
+						}while (ID<1000 || ID>9999);
+						archivo << ID <<",";
+						archivo <<s[1]<<","<<s[2]<<","<<s[3];
+						break;
+					}
+					case 2:{
+						do{
+							cout << "escribe por que quieres cambiar el SubID"<<endl;
+							cin >> SubID;
+							fflush(stdin);
+							if(SubID<01 || SubID>99)
+								cout << " el SUBID se rige es de 2 digitos ";
+						}while(SubID<01 || SubID>99);
+						archivo << s[0]<<",";
+						archivo <<SubID<<",";
+						archivo <<s[2]<<","<<s[3];
+						break;
+					}
+					case 3:{
+						cout << "escribe por que quieres cambiar el nombre"<<endl;
+						cin >> Name;
+						fflush(stdin);
+						archivo << s[0]<<","<<s[1]<<",";
+						archivo <<Name<<",";
+						archivo <<s[3];
+						break;
+					}
+					case 4:{
+						do{
+							cout << "escribe por que quieres cambiar el precio"<<endl;
+							cin >> precio;
+							fflush(stdin);
+							if(precio<0)
+								cout << " el precio siempre tiene que ser entero "<<endl;
+						}while(precio<01 || precio>99);
+						archivo << s[0]<<","<<s[1]<<","<<s[3]<<",";
+						archivo <<precio;
+						break;
+					}
+				}
+			}
+			pos=archivo.tellg();
+		}
 	}
-
 	archivo.close();
-	Temp.close();
-	remove("Agregar.txt");
-	rename("Temp.txt", "Agregar.txt");
-	system("pause");
 	system("cls");
 	menu();
 }
 
 string CrearNota() {
 	system("cls");
-	int ID, op3, op4, i, Bclave;
-	char Nombre[40];
-	float precio, Psuma;
-	ifstream archivo;
-	ofstream Temp2;
-	Psuma = 0;
-
-	do {
-		cout << "Ha escogido la opcion de Crear Nota " << endl;
-		cout << "1.Crear su Nota \n2. Regresar al Menu Principal" << endl;
-		bool encontrado = false;
-		cin >> op3;
+	int i,ops1,ops2,ops3,strAsInt,strAsInt2,ops4;
+	ofstream temp("ticket.txt",ios::out);
+	cout << " acabas de Ingresar a la Seccion de Crear Nota "<<endl;
+	do{
+		
+	do{
+		cout << "Ingresa la ID del Servicio a Usar"<<endl;
+		cin >> ops1;
 		fflush(stdin);
-		system("cls");
-
-		if (op3 == 1) {
-			archivo.open("Agregar.txt", ios::app);
-			Temp2.open("Temp.txt");
-			cout << "Ingrese la cantidad de Servicios que desea utilizar" << endl;
-			cin >> op4;
-			fflush(stdin);
-			for (i = 1; i <= op4; i++);
-			cout << "Ingrese el ID del  Servicio\n";
-			cin >> ID;
-			cout << "Ingrese el precio estimado del servicio" << endl;
-			cin >> precio;
-			fflush(stdin);
-			while (archivo.eof()) {
-
-				archivo >> Bclave;
-				if (ID == Bclave) {
-					encontrado = true;
-					cout << "El nombre del Servicio es:" << Nombre << endl;
-					cout << " El ID del servicio es:" << ID << endl;
-					cout << " El Precio del servicio es" << precio << endl;
-					for (i = 1; i <= op4; i++) {
-						Psuma = Psuma + precio;
-					}
-					cout << "La nota fue creada con exito\n\n";
-					system("cls");
-					cout << "La Nota es:" << endl;
-					cout << Nombre << ID << endl;
-					cout << "El total es:" << Psuma << endl;
-				}
-				archivo >> Nombre;
-				if (encontrado = false) {
-					cout << "El ID no fue encontrado en el registro" << endl;
-				}
-				archivo.close();
-				Temp2.close();
-				remove("Agregar.txt");
-				rename("Temp.txt", "Agregar.txt");
-				system("Pause");
-				system("cls");
+		cout <<"ingresa el SUBID a Usar"<<endl;
+		cin >> ops2;
+		fflush(stdin);
+		cout << " Usaras la ID "<<ops1 << "y la SubID "<< ops2<<endl;
+		cout << "es correcto? [1]-si	/	[2]-no"<<endl;
+		cin >> ops3;
+		fflush(stdin);
+	}while(ops3==2);
+	fstream archivo("Servicio.txt",ios::in);
+	while(!archivo.eof()){
+		string s[4];
+		string line;
+		int pos = archivo.tellg();
+		if(line.length()>0){
+			stringstream sline(line);
+			for(i=0;i<4;i++){
+				getline(sline,s[i],',');
+				if(i==0)
+					sline>>strAsInt;
+				if(i==1)
+					sline>>strAsInt2;
+			}
+			if(strAsInt==ops1 && strAsInt2==ops2){
+				cout << "|"<<s[i];
+				temp << s[0] << "," << s[1] << "," << s[2] << "," << s[3];
 			}
 		}
-	} while (op3 != 2);
+	}
+	archivo.close();
+	cout <<"quieres ingresar mas servicios y subservicios a utilizar?\n[1]-si\n[2]-no"<<endl;
+	cin >>ops4;
+}while(ops4==2);
+	temp.close();
 	menu();
 }
 
 string ConsultarNota() {
 	system("cls");
 	cout << "Ha escogido la opcion de Consulta de Notas\n" << endl;
-	ifstream Temp2;
-	string texto;
-
-	Temp2.open("Temp2.txt", ios::app);
-
-	if (Temp2.fail()) {
-		cout << "no se pudo abrir el archivo" << endl;
-		exit(1);
-	}
-	while (!Temp2.eof()) {
-		getline(Temp2, texto);
-		cout << texto << endl;
-	}
-	Temp2.close();
+	
 	menu();
 }
 
